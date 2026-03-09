5a7aa8352251c990591b38a4ba36f3ff13c66b93
FairyTalePol <egorusdnepr@gmail.com>
2026-03-09 16:29:48 +0200

    Added a lot of stuff

--- src/main/kotlin/MailAggregator/MailAggregator/telegram/TelegramConfig.kt
+++ src/main/kotlin/MailAggregator/MailAggregator/telegram/TelegramConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class TelegramConfig {
-
     @Bean
     fun categorizationBot(
         handleTelegramResponseUseCase: HandleTelegramResponseUseCase,
